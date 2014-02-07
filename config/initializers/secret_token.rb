7fca8cbe86a18bdc0e413bca1b0700a63ea06a78
Tom Cartwright <tecartwright@gmail.com>
2014-02-07 09:06:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Floader::Application.config.secret_key_base = '34c88083305dc3e1771967085410e8c6acbd41187ac3bb2a2587e908abc60930ed9c15a3b3c8b7b27b439fa94958652b07a186405693aa5c4a82b6eb7413e10e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Floader::Application.config.session_store :cookie_store, key: '_floader_session'
